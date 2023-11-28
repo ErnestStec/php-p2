8344ac1b725c9693e7370a96426b6e5ed6b0ef2f
ErnestStec <144111990+ErnestStec@users.noreply.github.com>
2023-11-28 09:17:28 +0100

    Add files via upload

---
+++ opdracht3.php
@@ -0,0 +1,18 @@
+<?php
+  // Auteur: Ernest
+  // Functie: Opdracht 3
+
+  // Initalisatie:
+
+  // Main:
+
+  $string1 = "Hello";
+  $string2 = "World";
+  $nummer1 = 5;
+  $nummer2 = 10;
+  $nummer3 = 15;
+  echo $string1, $string2;
+  echo "<br>";
+  echo $nummer1 + $nummer2 = $nummer3;
+
+  ?>
---
+++ telop.php
@@ -0,0 +1,19 @@
+<?php
+  // Auteur: Ernest
+  // Functie: Rekenen 4.36
+  $getal1 = 10;
+  $getal2 = 10;
+  // Optellen
+  $uitkomst = 1000 + 2000;
+  echo "Uitkomst = $uitkomst"; 
+  echo "<br>";
+  // Verhogen met 10%
+  $uitkomst = $uitkomst / 100 * 110;
+  echo "Uitkomst = $uitkomst";
+  echo "<br>";
+  // Trek er 1000 vanaf
+  $uitkomst = $uitkomst - 1000; 
+  echo "Uitkomst = $uitkomst";
+  
+  ?>
+  
